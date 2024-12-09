513af2aa6e71654ae982ae0928981d1b3ae547ab
Zachary Hardin <zacharyjhardin@yahoo.com>
2024-12-08 19:31:43 -0600

    day 8: part 2 implementation

--- day 8/day8.cbl
+++ day 8/day8.cbl
@@ -51,9 +51,17 @@
 
        01  WS-POSS-AN-SUB                         PIC S9(2).
        01  WS-POSS-AN-CHAR                        PIC S9(2).
+       01  WS-POS1-SUB                            PIC  9(2).
+       01  WS-POS1-CHAR                           PIC  9(2).
+       01  WS-POS2-SUB                            PIC  9(2).
+       01  WS-POS2-CHAR                           PIC  9(2).       
        01  WS-ANTINODES-ARR. 
            05 WS-ANTINODES OCCURS 12 TO 50 TIMES DEPENDING ON ARR-LENGTH
                                                   PIC X(140).     
+       01  WS-ANTENNAS-ARR.
+           05 WS-ANTENNAS  OCCURS 500             PIC X(1).
+       01  ANT-SUB                                PIC 9(3).
+       01  WS-ANT-CNT                             PIC 9(3).
             
        01  WS-ANTINODES-CNT                       PIC 9(4)  VALUE 0.  
 
@@ -70,6 +78,7 @@
            PERFORM 2000-CONVERT-FILE-TO-ARRAY THRU 2000-EXIT
                UNTIL END-OF-FILE     
            PERFORM 3000-IDENTIFY-PAIRS        THRU 3000-EXIT
+           PERFORM 7000-ADD-VALID-ANTENNAS    THRU 7000-EXIT
            PERFORM 8000-DISPLAY-RESULTS       THRU 8000-EXIT
            PERFORM 9000-CLOSE-FILE            THRU 9000-EXIT
            .
@@ -109,7 +118,8 @@
       *****************************************************************
        3000-IDENTIFY-PAIRS.
 
-           MOVE SPACES TO WS-ANTINODES-ARR       
+           MOVE SPACES TO WS-ANTINODES-ARR 
+                          WS-ANTENNAS-ARR      
            MOVE 1      TO MAP-ARR-SUB
 
            PERFORM UNTIL MAP-ARR-SUB EQUALS (ARR-LENGTH + 1)
@@ -161,34 +171,92 @@
               MAP-SUB-CHAR EQUALS MAP-SUB-CHAR2
                GO TO 3020-EXIT
            END-IF
+         
+           MOVE MAP-ARR-SUB     TO WS-POS1-SUB
+           MOVE MAP-SUB-CHAR    TO WS-POS1-CHAR
+           MOVE MAP-ARR-SUB2    TO WS-POS2-SUB
+           MOVE MAP-SUB-CHAR2   TO WS-POS2-CHAR
+           .
 
-           SUBTRACT MAP-ARR-SUB2  FROM MAP-ARR-SUB GIVING WS-PAIR-DIFF-X
-           SUBTRACT MAP-SUB-CHAR2 FROM MAP-SUB-CHAR 
+           CHECK-FOR-ANTINODE.
+           
+           SUBTRACT WS-POS2-SUB  FROM WS-POS1-SUB GIVING WS-PAIR-DIFF-X
+           SUBTRACT WS-POS2-CHAR FROM WS-POS1-CHAR 
                                                    GIVING WS-PAIR-DIFF-Y                                                 
 
-           ADD MAP-ARR-SUB  WS-PAIR-DIFF-X GIVING WS-POSS-AN-SUB
-           ADD MAP-SUB-CHAR WS-PAIR-DIFF-Y GIVING WS-POSS-AN-CHAR                                    
-           
+           ADD WS-POS1-SUB  WS-PAIR-DIFF-X GIVING WS-POSS-AN-SUB
+           ADD WS-POS1-CHAR WS-PAIR-DIFF-Y GIVING WS-POSS-AN-CHAR                                      
+
            IF (WS-POSS-AN-SUB) GREATER 0              AND
               (WS-POSS-AN-SUB) LESS (ARR-LENGTH + 1)  AND
               (WS-POSS-AN-CHAR) GREATER 0             AND
-              (WS-POSS-AN-CHAR) LESS (ARR-LENGTH + 1)
-
+              (WS-POSS-AN-CHAR) LESS (ARR-LENGTH + 1) 
       *        CHECK IF LOCATION ALREADY HAS ANTINODE BEFORE INCREMENT    
                IF WS-ANTINODES(WS-POSS-AN-SUB)(WS-POSS-AN-CHAR:1) 
                   EQUALS 'Y'
                    CONTINUE
-               ELSE        
-                   MOVE 'Y' TO 
+               ELSE 
+      *            CHECK IF ANTENNA ALREADY EXISTS ON SELECTED NODE
+      *            (WAS FINE HERE IN PART 1, BUT THAT NOW IS HANDLED BY
+      *             PARA 7000, SO ADDING CHECK HERE TO NOT COUNT TWICE)
+                   IF WS-MAP(WS-POSS-AN-SUB)(WS-POSS-AN-CHAR:1)
+                      EQUALS '.' 
+                       ADD 1 TO WS-ANTINODES-CNT                     
+                       MOVE 'Y' TO 
                          WS-ANTINODES(WS-POSS-AN-SUB)(WS-POSS-AN-CHAR:1) 
-                   ADD 1 TO WS-ANTINODES-CNT
+                       PERFORM 3025-ADD-ANTENNA THRU 3025-EXIT
+                   END-IF
                END-IF
+                   MOVE WS-POS1-SUB     TO WS-POS2-SUB
+                   MOVE WS-POS1-CHAR    TO WS-POS2-CHAR
+                   MOVE WS-POSS-AN-SUB  TO WS-POS1-SUB
+                   MOVE WS-POSS-AN-CHAR TO WS-POS1-CHAR
+                   GO TO CHECK-FOR-ANTINODE       
            END-IF
            
            .
        3020-EXIT.
            EXIT. 
 
+      *****************************************************************
+      * IF IT ISN'T ALREADY, ADD ANTENNA TO UNIQUE ARRAY AND ADD TO   *
+      * OUTPUT COUNTER                                                *
+      *****************************************************************
+       3025-ADD-ANTENNA.
+
+           MOVE 1 TO ANT-SUB
+           PERFORM UNTIL WS-ANTENNAS(ANT-SUB) EQUALS SPACE
+               IF WS-ANTENNAS(ANT-SUB) EQUALS WS-FREQ
+                   GO TO 3025-EXIT
+               END-IF
+               ADD 1 TO ANT-SUB
+           END-PERFORM
+      
+      *    REACHING THIS INSTRUCTION MEANS THE FREQUENCY THAT CREATED
+      *    AN ANTINODE IS "NEW" AND NEEDS TO BE ACCOUNTED FOR
+           MOVE WS-FREQ TO WS-ANTENNAS(ANT-SUB)
+           .
+       3025-EXIT.
+           EXIT.
+           
+      *****************************************************************
+      * ADD 1 UNIT TO OUTPUT COUNTER FOR EVERY ANTENNA THAT PRODUCED  *
+      * AN ANTINODE.                                                  *
+      *****************************************************************
+       7000-ADD-VALID-ANTENNAS.
+
+           MOVE 1 TO ANT-SUB      
+                     
+           PERFORM UNTIL WS-ANTENNAS(ANT-SUB) EQUALS SPACE
+               MOVE 0 TO WS-ANT-CNT
+               INSPECT WS-MAP-ARR
+               TALLYING WS-ANT-CNT FOR ALL WS-ANTENNAS(ANT-SUB)
+               ADD WS-ANT-CNT TO WS-ANTINODES-CNT
+               ADD 1 TO ANT-SUB
+           END-PERFORM
+           .
+       7000-EXIT.
+           EXIT.
       *****************************************************************
       * DISPLAY RESULTING SUM OF PRODUCTS                             *
       *****************************************************************
